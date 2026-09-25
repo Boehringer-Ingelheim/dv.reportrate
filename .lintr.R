9577444639bbb1b9c0d629376bcff9246d2e4675
typoumet <methodios.typou.ext@boehringer-ingelheim.com>
2026-09-25 13:41:11 +0100

    Refactor lintr configuration in .lintr.R

--- .lintr.R
+++ .lintr.R
@@ -1,18 +1,14 @@
-message("lintr configuration file loaded ")
-
-# an dv.papo orientiert
+linters <- lintr::default_linters             # -[ diff with dv.templates 3ca8d7a10cfc7ad2307644dcac603e1f1f0feb72]-
 linters <- lintr::modify_defaults(
-  lintr::default_linters
-  , line_length_linter = lintr::line_length_linter(120L)
-  #, return_linter = lintr::return_linter(return_style = "explicit")
-  , indentation_linter = NULL # Weite der Einrückungen wird ignoriert
-  #, object_usage_linter = NULL # hier noch mal auskommentieren und wegen den Funktionsparametern gucken.
-  #, trailing_whitespace_linter = NULL
-  #, cyclocomp_linter = NULL                   # prevents trivial amount of nesting and long but straightforward functions
-  #, object_name_linter = NULL                 # we have reasons to capitalize. nobody in our team CamelCase. shiny does
-  #, object_length_linter = NULL               # we don't type long var names just because
-  #, pipe_continuation_linter = NULL           # wickham being overly prescriptive
-  #, trailing_blank_lines_linter = NULL        # natural extension of trailing_whitespace_linter, present on the template
+  linters
+  , line_length_linter = NULL                 # we see how long lines are when we write them
+  , indentation_linter = NULL
+  , trailing_whitespace_linter = NULL
+  , object_name_linter = NULL                 # we have reasons to capitalize. nobody in our team CamelCase. shiny does
+  , object_length_linter = NULL               # we don't type long var names just because
+  , pipe_continuation_linter = NULL           # wickham being overly prescriptive
+  , trailing_blank_lines_linter = NULL        # natural extension of trailing_whitespace_linter, present on the template
+  , return_linter = NULL                      # wickham being overly prescriptive
 )
 
 if(identical(Sys.getenv('CI'), "true")){
@@ -20,6 +16,6 @@ if(identical(Sys.getenv('CI'), "true")){
     linters
     , object_usage_linter = NULL              # R lacks var declarations; it's easy to assign to the wrong variable by mistake
   )                                           # We only disable this lint rule on github because it fails there because
-}
+}                                             # of a long-standing lintr bug
 
-linters
+exclusions <- list("tests")
